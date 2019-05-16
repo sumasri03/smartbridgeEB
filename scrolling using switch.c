5ed3d02e649df1f3861a0bb9e2d6c25f8a5a7596
sumasri03 <50613614+sumasri03@users.noreply.github.com>
2019-05-16 18:54:36 +0530

    Add files via upload

---
+++ scrolling using switch.c
@@ -0,0 +1,39 @@
+
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRA=0XFF;
+	//DDRC=0X01;
+	DDRD&=~(1<<PD0);
+	DDRD&=~(1<<PD1);
+	//DDRD=0X00;
+    while (1) 
+    {
+		if((PIND&(1<<PD0))==0){
+			for(int i=PA0;i<=PA7;i++){
+				PORTA=1<<i;
+				_delay_ms(500);
+	
+			}	
+		}
+		if((PIND&(1<<PD1))==0){
+			for(int i=PA7;i<=PA0;i++){
+				PORTA=1<<i;
+				_delay_ms(500);
+				
+			}	
+			
+			
+			
+						
+		}
+    }
+}
+
